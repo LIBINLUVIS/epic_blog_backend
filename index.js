870e29d04959cda500fee6385de199edb3b774fe
Libin Luvis <libinluvis10@gmail.com>
2022-10-11 00:49:44 +0530

    the port switched

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const connectToMongo = require("./db")
 const express = require("express")
 const app = express()
 const cors = require("cors")
-const port = 5000
+const port = 80
 
 connectToMongo()
 
