86734a31b48636d038b7e8c3743eb9dfa1f10d5a
LIBINLUVIS <libinluvis10@gmail.com>
2022-12-05 00:19:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const connectToMongo = require("./db")
 const express = require("express")
 const app = express()
 const cors = require("cors")
-const port = 8080
+const port = 80
 
 connectToMongo()
 
